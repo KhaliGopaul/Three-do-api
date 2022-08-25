220d6ad7c3cea36140695d9bb3c8b62a99b94ec7
Khali Gopaul <khali.gopaul@gmail.com>
2022-08-25 11:57:39 -0400

    three-do-api

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+import functions from "firebase-functions";
+import express from 'express';
+import cors from 'cors';
+import { getTasks, createTask, updateTask, deleteTask } from './src/tasks.js';
+
+const app = express();
+app.use(cors());
+app.use(exporess.json());
+
+app.get('/tasks', getTasks);
+app.post('/tasks', createTask);
+app.patch('./tasks/:taskId', updateTask);
+app.delete('/takss/:taskId', deleteTask);
+
+
+
+export const api = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
