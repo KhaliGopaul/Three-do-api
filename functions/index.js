b516dd75c1149515aff3139649996b87500501a2
Khali Gopaul <khali.gopaul@gmail.com>
2022-09-01 18:08:15 -0400

    added tasks

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
-import functions from "firebase-functions";
+import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
-import { getTasks, createTask, updateTask, deleteTask } from './src/tasks.js';
+import { getTasks, createTask, updateTask, deleteTask }
+  from './src/tasks.js';
+import { createUser, loginUser } from './src/users.js';
 
 const app = express();
 app.use(cors());
@@ -9,17 +11,10 @@ app.use(express.json());
 
 app.get('/tasks', getTasks);
 app.post('/tasks', createTask);
-app.patch('./tasks/:taskId', updateTask);
+app.patch('/tasks/:taskId', updateTask);
 app.delete('/tasks/:taskId', deleteTask);
 
-
+app.post('/users', createUser);
+app.post('/users/login', loginUser);
 
 export const api = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,12 +1,17 @@
-import { dbConnect } from "./dbConnect.js";
+import jwt from 'jsonwebtoken';
+import dbConnect from './dbConnect.js';
+import { secretKey } from '../credentials.js';
 
 export async function getTasks(req, res) {
+  const token = req.headers.authorization;
+  const user = jwt.verify(token, secretKey);
   const db = dbConnect();
-  const collection = await db
-    .collection("tasks")
+  const collection = await db.collection('tasks')
+    .where('userId', '==', user.id)
     .get()
-    .catch((err) => res.status(500).send(err));
-  const tasks = collection.docs.map((doc) => {
+      .catch(err => res.status(500).send(err));
+  const tasks = collection.docs.map(doc => {
+    // return {...doc.data(), id: doc.id }
     let task = doc.data();
     task.id = doc.id;
     return task;
@@ -15,18 +20,19 @@ export async function getTasks(req, res) {
 }
 
 export async function createTask(req, res) {
-  const newTask = req.body;
-  if (!newTask || !newTask.task) {
-    res.status(400).send({ succes: false, message: "Invalid request" });
+  const token = req.headers.authorization;
+  let newTask = req.body;
+  const user = jwt.verify(token, secretKey);
+  if (!newTask || !newTask.task || !user) {
+    res.status(400).send({ success: false, message: 'Invalid request' });
     return;
   }
+  newTask.userId = user.id;
   const db = dbConnect();
-  await db
-    .collection("tasks")
-    .add(newTask)
-    .catch((err) => res.status(500).send(err));
+  await db.collection('tasks').add(newTask)
+    .catch(err => res.status(500).send(err));
   res.status(201);
-  getTasks(req, res);
+  getTasks(req, res); // send back the full list of tasks...
 }
 
 export async function updateTask(req, res) {
@@ -34,12 +40,12 @@ export async function updateTask(req, res) {
   const { taskId } = req.params;
   const db = dbConnect();
   await db.collection('tasks').doc(taskId).update(taskUpdate)
-  .catch(err => res.status(500).send(err));
+    .catch(err => res.status(500).send(err));
   res.status(202);
   getTasks(req, res);
 }
 
 export function deleteTask(req, res) {
   const { taskId } = req.params;
-  res.status(203).send("Task Deleted");
+  res.status(203).send('Task Deleted');
 }
